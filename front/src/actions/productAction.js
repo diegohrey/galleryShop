4f01dc187f1a3530636540d600b248504e34fada
Andresaki <andrespradah@gmail.com>
2022-11-13 12:59:17 -0500

    actualizar usuario-contraseña etc

--- front/src/actions/productAction.js
+++ front/src/actions/productAction.js
@@ -3,62 +3,56 @@ import axios from 'axios';
 import {
     ALL_PRODUCTS_REQUEST,
     ALL_PRODUCTS_SUCCESS,
-    ALL_PRODUCTS_FAIL, 
+    ALL_PRODUCTS_FAIL,
     PRODUCT_DETAILS_REQUEST,
     PRODUCT_DETAILS_SUCCESS,
     PRODUCT_DETAILS_FAIL,
     CLEAR_ERRORS
-} from '../constants/productConstants.js';
+} from '../constants/productConstants';
 
 export const getProducts = ( currentPage =1, keyword='', precio) => async(dispatch)=>{
     try {
-        dispatch({ type: ALL_PRODUCTS_REQUEST })
+        dispatch({type: ALL_PRODUCTS_REQUEST})
 
         let link=`/api/productos?keyword=${keyword}&page=${currentPage}&precio[gte]=${precio[0]}&precio[lte]=${precio[1]}`
 
         const {data} = await axios.get(link)
 
         dispatch({
-            type: ALL_PRODUCTS_SUCCESS,
+            type:ALL_PRODUCTS_SUCCESS,
             payload: data
         })
-
-    } catch (error) {
+    }catch (error){
         dispatch({
-            type: ALL_PRODUCTS_FAIL,
+            type:ALL_PRODUCTS_FAIL,
             payload: error.response.data.message
         })
-
     }
 }
 
-// VER DETALLE DEL PRODUCTO
-
-export const getProductDetails = (id) => async (dispatch) => {
+//VER DETALLE DEL PRODUCTO
+export const getProductDetails = (id) => async(dispatch)=>{
     try {
-        dispatch({ type: PRODUCT_DETAILS_REQUEST })
+        dispatch({type: PRODUCT_DETAILS_REQUEST})
 
-        const { data } = await axios.get(`/api/producto/${id}`)
+        const {data} = await axios.get(`/api/producto/${id}`)
 
         dispatch({
-            type: PRODUCT_DETAILS_SUCCESS,
+            type:PRODUCT_DETAILS_SUCCESS,
             payload: data.product
         })
-
-    } catch (error) {
+    }catch (error){
         dispatch({
-            type: PRODUCT_DETAILS_FAIL,
+            type:PRODUCT_DETAILS_FAIL,
             payload: error.response.data.message
         })
-
     }
 }
 
 
-// CLEAR ERROR
-export const clearErrors = () => async (dispatch) => {
+//clear error
+export const clearErrors = () => async(dispatch)=>{
     dispatch({
-        type: CLEAR_ERRORS
+        type:CLEAR_ERRORS
     })
 }
-
